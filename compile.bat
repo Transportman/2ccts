f7f1cfa8745e53a0f2b4ce54ac7e5341cbcc5e59
Transportman <47077527+Transportman@users.noreply.github.com>
2014-08-17 14:18:58 +0200

    Update: Compile .bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,8 @@
+@echo off
+cls
+:start
 make
-pause
+set choice=
+set /p choice="Do you want to restart make process? Press 'y' and enter for Yes: "
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='y' goto start
