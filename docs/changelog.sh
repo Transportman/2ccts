95004b1364311cb7ce0922c27b08329cf41a023f
Transportman <47077527+Transportman@users.noreply.github.com>
2014-09-26 19:38:57 +0200

    Doc: Changelog for 1.0-beta5

--- docs/changelog.sh
+++ docs/changelog.sh
@@ -3,4 +3,4 @@
 #!/bin/sh
 
 #Create list of constants and variables
-hg log -r 322:tip --template '- {desc}\n' | sort > changelog_raw.txt
+hg log -r 346:tip --template '- {desc}\n' | sort > changelog_raw.txt
