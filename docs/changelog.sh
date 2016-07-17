2741b2a96c22fe3ab49ecdb712847a9b6df54fae
Transportman <47077527+Transportman@users.noreply.github.com>
2016-07-17 16:21:59 +0200

    Update r777: Correct changelog for 2.0-alpha1 (closes #8223)

--- docs/changelog.sh
+++ docs/changelog.sh
@@ -3,4 +3,4 @@
 #!/bin/sh
 
 #Create list of constants and variables
-hg log -r 478:tip --template '- {desc}\n' | sort > changelog_raw.txt
+hg log -r 778:tip --template '- {desc}\n' | sort > changelog_raw.txt
